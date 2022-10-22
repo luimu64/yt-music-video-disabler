ed78ccc04aaa56453ca6adb41612b9c074e2135f
luimu64 <luimu@luimu.dev>
2022-10-22 17:34:46 +0300

    initial commit

---
+++ disabler.js
@@ -0,0 +1,9 @@
+
+const leftPanel = document.getElementById("main-panel")
+const rightPanel = document.getElementsByClassName("side-panel modular style-scope ytmusic-player-page")[0]
+
+leftPanel.hidden = true
+
+rightPanel.style.margin = "0"
+rightPanel.style.width = "100%"
+rightPanel.style.maxWidth = "unset"
